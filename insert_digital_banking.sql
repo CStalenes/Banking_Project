feadd6d3a3f8757ad8e9021aa1cc1ed11c97b368
SCoriolan <stalenes.coriolan@gmail.com>
2026-02-19 23:32:28 +0100

    Add payment_card entries to insert_digital_banking.sql

--- insert_digital_banking.sql
+++ insert_digital_banking.sql
@@ -1,3 +1,4 @@
+--- TABLE TRANSACTIONS ---
 INSERT INTO transactions (id, amount, date_transac, source_account_id, destination_account_id, type_transaction_id) VALUES (1, 5950447, '1/14/2026', 1, 1, 1);
 INSERT INTO transactions (id, amount, date_transac, source_account_id, destination_account_id, type_transaction_id) VALUES (2, 9593365, '5/30/2025', 2, 2, 2);
 INSERT INTO transactions (id, amount, date_transac, source_account_id, destination_account_id, type_transaction_id) VALUES (3, 4579501, '5/18/2025', 3, 3, 3);
@@ -13,3 +14,20 @@ INSERT INTO transactions (id, amount, date_transac, source_account_id, destinati
 INSERT INTO transactions (id, amount, date_transac, source_account_id, destination_account_id, type_transaction_id) VALUES (13, 9734962, '8/8/2025', 13, 13, 13);
 INSERT INTO transactions (id, amount, date_transac, source_account_id, destination_account_id, type_transaction_id) VALUES (14, 6269428, '1/24/2026', 14, 14, 14);
 INSERT INTO transactions (id, amount, date_transac, source_account_id, destination_account_id, type_transaction_id) VALUES (15, 5720105, '8/19/2025', 15, 15, 15);
+
+
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (1, 1, 1, 'online', 'China', false, '6/9/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (2, 2, 2, 'contactless payement', 'China', false, '3/14/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (3, 3, 3, 'contactless payement', 'Indonesia', true, '7/23/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (4, 4, 4, 'contactless payement', 'Indonesia', true, '3/8/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (5, 5, 5, 'online', 'Sweden', false, '11/1/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (6, 6, 6, 'online', 'France', true, '2/21/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (7, 7, 7, 'online', 'Egypt', false, '3/13/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (8, 8, 8, 'contactless payement', 'Brazil', true, '8/8/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (9, 9, 9, 'contactless payement', 'Kazakhstan', true, '11/19/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (10, 10, 10, 'contactless payement', 'Sweden', false, '7/26/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (11, 11, 11, 'credit card', 'United States', false, '5/16/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (12, 12, 12, 'credit card', 'Argentina', false, '6/25/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (13, 13, 13, 'contactless payement', 'Russia', true, '7/24/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (14, 14, 14, 'credit card', 'Nicaragua', false, '7/25/2025');
+INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (15, 15, 15, 'online', 'Brazil', false, '8/18/2025');
