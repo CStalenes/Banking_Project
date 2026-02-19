49818155cb2cba4f63725e5a44507ef6d571a631
SCoriolan <stalenes.coriolan@gmail.com>
2026-02-20 00:54:33 +0100

    Add  in entity audit log entries to insert_digital_banking.sql for improved transaction monitoring

--- insert_digital_banking.sql
+++ insert_digital_banking.sql
@@ -15,7 +15,7 @@ INSERT INTO transactions (id, amount, date_transac, source_account_id, destinati
 INSERT INTO transactions (id, amount, date_transac, source_account_id, destination_account_id, type_transaction_id) VALUES (14, 6269428, '1/24/2026', 14, 14, 14);
 INSERT INTO transactions (id, amount, date_transac, source_account_id, destination_account_id, type_transaction_id) VALUES (15, 5720105, '8/19/2025', 15, 15, 15);
 
-
+--- TABLE PAYMENT CARD ---
 INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (1, 1, 1, 'online', 'China', SUCCESS, '6/9/2025');
 INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (2, 2, 2, 'contactless payement', 'China', SUCCESS, '3/14/2025');
 INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (3, 3, 3, 'contactless payement', 'Indonesia', SKIPPED, '7/23/2025');
@@ -32,7 +32,7 @@ INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, m
 INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (14, 14, 14, 'credit card', 'Nicaragua', SUCCESS, '7/25/2025');
 INSERT INTO payment_card (id, transaction_id, banking_card_id, payment_method, merchant_country, three_ds_status, payment_date) VALUES (15, 15, 15, 'online', 'Brazil', FAILED, '8/18/2025');
 
-
+--- TABLE TRANSFER ---
 INSERT INTO transfer (id, transaction_id, beneficiary_id, account_id, reference, reason, creation_date, execution_date, fees) VALUES (1, 1, 1, 1, '604191262-X', 'Morbi porttitor lorem id ligula. In est risus,', '1/13/2026', '2/27/2025', '$2.10');
 INSERT INTO transfer (id, transaction_id, beneficiary_id, account_id, reference, reason, creation_date, execution_date, fees) VALUES (2, 2, 2, 2, '902382231-A', 'Phasellus sit amet erat. Nulla tempus.', '1/29/2026', '12/9/2025', '$3.29');
 INSERT INTO transfer (id, transaction_id, beneficiary_id, account_id, reference, reason, creation_date, execution_date, fees) VALUES (3, 3, 3, 3, '716918656-X', 'Nulla ac enim. In tempor,', '9/11/2025', '1/7/2026', '$2.18');
@@ -47,3 +47,20 @@ INSERT INTO transfer (id, transaction_id, beneficiary_id, account_id, reference,
 INSERT INTO transfer (id, transaction_id, beneficiary_id, account_id, reference, reason, creation_date, execution_date, fees) VALUES (13, 13, 13, 13, '234658169-Y', 'Maecenas tristique, est et tempus semper.', '3/26/2025', '11/15/2025', '$3.33');
 INSERT INTO transfer (id, transaction_id, beneficiary_id, account_id, reference, reason, creation_date, execution_date, fees) VALUES (14, 14, 14, 14, '077292479-Z', 'Praesent blandit. Nam nulla.', '4/26/2025', '3/19/2025', '$1.04');
 INSERT INTO transfer (id, transaction_id, beneficiary_id, account_id, reference, reason, creation_date, execution_date, fees) VALUES (15, 15, 15, 15, '042637994-2', 'Aenean lectus. Pellentesque eget nunc leo.', '3/4/2025', '7/22/2025', '$4.69');
+
+--- TABLE AUDIT LOGS ---
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (1, 'add', 1, null,'500', '530', null, '7/1/2025', 1);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (2, 'add', 2, null,'500', '530', null, '10/10/2025', 2);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (3, 'add', 3, null,'500', '530', null, '6/16/2025', 3);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (4, 'update', 4, null,'50', '51', null, '1/18/2026', 4);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (5, 'delete', 5, null,'500', '0', null, '4/9/2025', 5);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (6, 'add', 6, null,'500', '530', null, '10/27/2025', 6);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (7, 'update', 7, null,'50', '51', null, '10/3/2025', 7);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (8, 'add', 8, null, '500', '530',null, '10/12/2025', 8);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (9, 'update', 9, null,'50', '51', null, '1/4/2026', 9);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (10, 'update', 10, null,'50', '51', null, '9/23/2025', 10);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (11, 'add', 11, null,'500', '530', null, '3/12/2025', 11);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (12, 'update', 12, null,'50', '51', null, '10/5/2025', 12);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (13, 'delete', 13, null,'500', '0', null, '3/10/2025', 13);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (14, 'update', 14, null,'50', '51', null, '8/27/2025', 14);
+INSERT INTO audit_logs (id, action_logs, customer_id, relevant_table, old_values, new_values, changed_fields, action_date, transaction_id) VALUES (15, 'update', 15, null,'50', '51', null, '4/10/2025', 15);
