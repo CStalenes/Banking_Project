cb8e409dbdd2095cea3060cfcaa9c5bb64801044
SCoriolan <stalenes.coriolan@gmail.com>
2026-02-20 15:41:37 +0100

    Add analytics queries to query_digital_banking.sql for top clients by transaction volume and average account balance ranking. Introduced CTEs for improved data aggregation and ranking functionality.

--- query_digital_banking.sql
+++ query_digital_banking.sql
@@ -39,3 +39,84 @@ END //
 DELIMITER ;
 
 -- CALL transfer_funds_update_transac(1, 2, 50);
+
+
+-- ANALYTICS QUERIES --
+
+/*Q1: Top 10 Clients by total transaction volume or total deposited amounts.*/
+
+WITH customer_volume AS (
+    SELECT 
+        c.id AS customer_id,
+        c.lastname,
+        c.firstname,
+        SUM(t.amount) AS total_volume,
+        COUNT(t.id) AS nb_transactions
+    FROM customers c
+    JOIN account a ON c.id = a.customer_id
+    JOIN transactions t ON a.id = t.source_account_id
+    GROUP BY c.id, c.lastname, c.firstname
+),
+
+ranked_clients AS (
+    SELECT
+        customer_id,
+        lastname,
+        firstname,
+        total_volume,
+        nb_transactions,
+        RANK() OVER (ORDER BY total_volume DESC) AS ranking
+    FROM customer_volume
+)
+
+SELECT
+    customer_id,
+    lastname,
+    firstname,
+    total_volume,
+    nb_transactions,
+    ranking
+FROM ranked_clients
+WHERE ranking <= 10
+ORDER BY ranking;
+
+/*Q2: Ranking with rank to display the average account balance */
+    -- (CTE + agrégation AVG + fenêtre RANK)
+
+-- Ranking by customer: average balance of their accounts
+WITH avg_balance_by_customer AS (
+    -- Agrégation : average balance by customer
+    SELECT
+        c.id AS customer_id,
+        c.lastname,
+        c.firstname,
+        COUNT(a.id) AS nb_account,
+        AVG(a.balance) AS avg_balance_acc
+    FROM customers c
+    JOIN account a ON a.customer_id = c.id
+    WHERE a.status_account = 'active'
+    GROUP BY c.id, c.lastname, c.firstname
+)
+SELECT
+    customer_id,
+    lastname,
+    firstname,
+    nb_account,
+    avg_balance_acc,
+    -- Window function: ranking by descending average balance
+    RANK()       OVER (ORDER BY avg_balance_acc DESC) AS rank_acc,
+    DENSE_RANK() OVER (ORDER BY avg_balance_acc DESC) AS dense_rank_acc,
+    ROW_NUMBER() OVER (ORDER BY avg_balance_acc DESC) AS row_rank_acc
+FROM avg_balance_by_customer
+ORDER BY avg_balance_acc DESC;
+
+
+-- OPTIMISATION QUERY --
+
+/*Q1*/
+
+/*Q1*/
+
+/*Q1*/
+
+/*Q1*/
